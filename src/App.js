6b7b902a79204d6439b5b452225acad79b3be790
Jason Hedrick <jasonleehedrick@gmail.com>
2021-02-03 14:02:18 -0800

    Finished Hello component to finish this basic project.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
+// Coding is a craft. Treat it as such.
+
 function App() {
   return (
     <div className="App">
-      
+      <Hello />
     </div>
   );
 }
 
+function Hello() {
+  return (
+    <div className="SayHi">
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
 export default App;
